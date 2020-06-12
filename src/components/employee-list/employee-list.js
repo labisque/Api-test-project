b3c570d91e84b32f89b58d36ea67e5dc29c7a48b
labisque <imlabisque@gmail.com>
2020-06-12 13:29:27 +0300

    Deleted variable assigned to JSX code in employee-list

--- src/components/employee-list/employee-list.js
+++ src/components/employee-list/employee-list.js
@@ -2,27 +2,25 @@ import React from 'react';
 import EmployeeListItem from '../employee-list-item/employee-list-item'
 import './employee-list.css'
 const EmployeeList = ({employeeArray, onDelete, onChange}) => {
-
-    const employees = employeeArray.map( (item) => {
-        const {id,...items} = item;
-
-        return (
-            <li key={id} className="list-group-item">
-                <EmployeeListItem
-                    {...items}
-                    id = {id}
-
-                    onDelete = {() => onDelete(id)}
-                    onChange = {onChange}
-                />
-            </li>
-        );
-    });
-
+    
     return (
         <div>
         <ul className="list-group list-container">
-            {employees}
+            {employeeArray.map( (item) => {
+                const {id,...items} = item;
+
+                return (
+                    <li key={id} className="list-group-item">
+                        <EmployeeListItem
+                            {...items}
+                            id = {id}
+
+                            onDelete = {() => onDelete(id)}
+                            onChange = {onChange}
+                        />
+                    </li>
+                );
+            })}
 
         </ul>
         </div>
